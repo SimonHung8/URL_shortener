7ec2b0de2d6e2984d19ed91058bb9358818aa8cc
SimonHung8 <simon850226@gmail.com>
2022-09-22 14:11:06 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,11 @@
+// require packages used in the project
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+// setting routes
+app.get('/', (req, res) => res.send('project init: URL_shortener'))
+
+// start and listen on the server
+app.listen(port, () => console.log(`App is running on http://localhost:${port}`))
