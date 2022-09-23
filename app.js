0314d1c33d3e89eedecee833b9f2bee695162ccc
SimonHung8 <simon850226@gmail.com>
2022-09-23 11:21:25 +0800

    feat: add handlebars and index page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // require packages used in the project
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 // database connection
 require('./config/mongoose')
@@ -7,8 +8,15 @@ require('./config/mongoose')
 const app = express()
 const port = 3000
 
+// setting template engine
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
+// setting static css file
+app.use(express.static('public'))
+
 // setting routes
-app.get('/', (req, res) => res.send('project init: URL_shortener'))
+app.get('/', (req, res) => res.render('index'))
 
 // start and listen on the server
 app.listen(port, () => console.log(`App is running on http://localhost:${port}`))
